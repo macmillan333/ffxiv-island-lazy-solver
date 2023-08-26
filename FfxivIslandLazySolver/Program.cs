c83126fb76237f546974722b6a1f81ceb292245d
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2023-08-25 23:00:16 -0700

    Rest of the reading.

--- FfxivIslandLazySolver/Program.cs
+++ FfxivIslandLazySolver/Program.cs
@@ -7,11 +7,17 @@ class Item
     {
         Material,
         RareMaterial,
+        GardeningStarter,
         Produce,
         Leavings
     }
-    public string? name;
+    public string name;
     public Type type;
+    public Item(string name, Type type)
+    {
+        this.name = name;
+        this.type = type;
+    }
 }
 
 class Handicraft
@@ -36,24 +42,43 @@ class Handicraft
         MarineMerchandise,
         UnburiedTreasures
     }
-    public string? name;
+    public string name;
     public int time;
     public int value;
     public Category category1;
     public Category category2;
     public class Ingredient
     {
-        public Item? item;
+        public Item item;
         public int quantity;
+
+        public Ingredient(Item item, int quantity)
+        {
+            this.item = item;
+            this.quantity = quantity;
+        }
     }
-    public List<Ingredient>? ingredients;
+    public List<Ingredient> ingredients;
+
+    public Handicraft(string name)
+    {
+        this.name = name;
+        this.ingredients = new List<Ingredient>();
+    }
 }
 
 class ExpeditionArea
 {
-    public string? terrain;
-    public List<string>? resources;
-    public string? rareResources;
+    public string terrain;
+    public List<Item> resources;
+    public Item rareResources;
+
+    public ExpeditionArea(string terrain, Item rareResources)
+    {
+        this.terrain = terrain;
+        this.resources = new List<Item>();
+        this.rareResources = rareResources;
+    }
 }
 
 class Program
@@ -61,25 +86,70 @@ class Program
     static void Main(string[] args)
     {
         const string kItemsFilename = "../../../Items.csv";
+        const string kGranaryFilename = "../../../Granary.csv";
+        const string kHandicraftFilename = "../../../Handicraft.csv";
 
         Dictionary<string, Item> items = new Dictionary<string, Item>();
         using (var streamReader = new StreamReader(kItemsFilename))
         {
             CsvReader reader = new CsvReader(streamReader, ",");
-            reader.Read(); // header
+            reader.Read();  // header
             while (reader.Read())
             {
                 string name = reader[0];
                 Item.Type type = Enum.Parse<Item.Type>(reader[1]);
-                items.Add(name, new Item()
-                {
-                    name = name, type = type
-                });
+                items.Add(name, new Item(name, type));
             }
         }
-        foreach (Item i in items.Values)
+
+        List<ExpeditionArea> areas = new List<ExpeditionArea>();
+        using (var streamReader = new
+            StreamReader(kGranaryFilename))
         {
-            Console.WriteLine($"{i.name} is a {i.type}");
+            CsvReader reader = new CsvReader(streamReader, ",");
+            reader.Read();  // header
+            while (reader.Read())
+            {
+                ExpeditionArea area = new ExpeditionArea(
+                    terrain: reader[0],
+                    rareResources: items[reader[1]]);
+                for (int i = 2; i < reader.FieldsCount; i++)
+                {
+                    if (reader[i] == "") break;
+                    area.resources.Add(items[reader[i]]);
+                }
+                areas.Add(area);
+            }
+        }
+
+        List<Handicraft> handicrafts = new List<Handicraft>();
+        using (var streamReader = new
+            StreamReader(kHandicraftFilename))
+        {
+            CsvReader reader = new CsvReader(streamReader, ",");
+            reader.Read();  // header
+            while (reader.Read())
+            {
+                Handicraft craft = new Handicraft(reader[0]);
+                craft.time = int.Parse(reader[1]);
+                craft.value = int.Parse(reader[2]);
+                craft.category1 = Enum.Parse<Handicraft.Category>(
+                    reader[3]);
+                craft.category2 = Handicraft.Category.None;
+                if (reader[4] != "")
+                {
+                    craft.category2 = Enum.Parse<Handicraft.Category>(
+                        reader[4]);
+                }
+                for (int i = 5; i < reader.FieldsCount; i += 2)
+                {
+                    if (reader[i] == "") break;
+                    craft.ingredients.Add(new Handicraft.Ingredient(
+                        item: items[reader[i]],
+                        quantity: int.Parse(reader[i + 1])));
+                }
+                handicrafts.Add(craft);
+            }
         }
     }
 }
